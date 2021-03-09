e833439d27e18e9cc17f6ac7b845fa9cac8b63fb
khs29733 <khs29733@gmail.com>
2021-03-09 09:48:36 +0900

    LinkedList, Array

---
+++ JavaEx/src/com/javaex/collection/list/ListEx.java
@@ -0,0 +1,63 @@
+package com.javaex.collection.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListEx {
+		public static void main(String[] args) {
+			listClassEx();
+		}
+		
+		private static void listClassEx() {
+			//List 관련 클래스는 List 인터페이스를 구현하고 있다
+			
+//			List<String> lst = new LinkedList<>();//실체 클래스는 LinkedList
+			List<String> lst = new ArrayList<>();
+			
+			System.out.println("lst :" + lst);
+			System.out.println("size of 1st : " + lst.size());
+			
+			//객체 추가
+			lst.add("Java");
+			lst.add("C");
+			lst.add("C++");
+			lst.add("Python");
+			
+			System.out.println("1st : " + lst);
+			
+			//객체 삽입
+			lst.add(2, "C#");
+			System.out.println("lst : " + lst);
+			
+			//List는 중복 삽입을 허용
+			lst.add("Java");
+			System.out.println("lst : " + lst);
+			
+			//조회
+			System.out.println("2번 인덱스 : " + lst.get(2));
+			
+			//루프
+			for(String item : lst) {
+				System.out.println(item + " ");
+			}
+			System.out.println();
+			
+			
+			//객체의 삭제
+			lst.remove(2);	//인덱스 이용 삭제
+			lst.remove("Python"); 	//객체 삭제
+			System.out.println("lst : " + lst);
+			
+			// 검색
+			int position = lst.indexOf("Java");
+			System.out.println("lst search : " + position);
+			position = lst.indexOf("Linux");	//없는 객체의 검색
+			System.out.println("없는 객체 검색 : " + position);
+		
+			//비우기
+			lst.clear();
+			System.out.println("lst : " + lst);
+		
+		
+		}
+}
